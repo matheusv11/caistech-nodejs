6c078995305202352be522fb2445593094536a3c
matheusv11 <mathsales360@gmail.com>
2022-07-02 15:08:34 -0300

    Rotas

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
 
+// CONTROLLERS
 const usuarioController = require('./controllers/usuarioController');
+const autenticacaoController = require('./controllers/autenticacaoController');
+
+// MIDDLEWARES
+const tokenValidador = require('./middlewares/tokenValidador');
+
+router.post('/autenticar', autenticacaoController.autenticar)
 
 router.get('/usuario', usuarioController.get)
+router.post('/criar-usuario', usuarioController.create)
+
+router.patch('/atualizar-usuario', tokenValidador.validarToken, usuarioController.update)
+router.delete('/deletar-usuario', tokenValidador.validarToken, usuarioController.delete)
 
 module.exports = router;
