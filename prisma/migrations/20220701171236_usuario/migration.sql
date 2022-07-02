47d9745b79f427c04ed92684c3693067e528701a
matheusv11 <mathsales360@gmail.com>
2022-07-02 15:08:30 -0300

    Sqlite

--- prisma/migrations/20220701171236_usuario/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "usuario" (
-    "id" SERIAL NOT NULL,
-    "nome" VARCHAR(100) NOT NULL,
-    "email" VARCHAR(100) NOT NULL,
-    "senha" VARCHAR(24) NOT NULL,
-
-    CONSTRAINT "usuario_pkey" PRIMARY KEY ("id")
-);
