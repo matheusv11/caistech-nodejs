bf493481721266293363093905d99052b6db2f1e
matheusv11 <mathsales360@gmail.com>
2022-07-02 15:08:44 -0300

    Middleware de validação jwt

---
+++ src/middlewares/tokenValidador.js
@@ -0,0 +1,15 @@
+const { verify: verificarToken } = require('jsonwebtoken');
+
+module.exports = {
+    validarToken(req, res, next) {
+        const token = req.headers.authorization?.split(" ")[1]
+
+        verificarToken(token, process.env.JWT_SECRET || 'secret123', (err, data) => {
+            if(err) return res.status(401).send({ erro: "Token inválido"})
+
+            res.locals.usuarioId = data.usuarioId
+            next()
+        })
+
+    }
+}
